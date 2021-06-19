d18a8f42d1033476e3974d91026cbab7c9400fd7
dirsulaiman <sulaiman.tsany@gmail.com>
2021-06-19 23:39:10 +0700

    controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use Inertia\Inertia;
+
 use Illuminate\Http\Request;
+use App\Models\Product as Model;
+use App\Models\ProductDetail;
+use App\Models\ProductImage;
 
 class ProductController extends Controller
 {
-    //
+    public function index () {
+        $model = Model::all();
+        return Inertia::render('Products', ['products' => $model]);
+    }
+
+    public function detail (Request $request, $id) {
+        return;
+    }
 }
