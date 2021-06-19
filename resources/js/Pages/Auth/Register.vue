7814eacdf002fabdb7aaf0c58af16def0c533b88
dirsulaiman <sulaiman.tsany@gmail.com>
2021-06-19 23:39:49 +0700

    resources

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,55 +1,62 @@
 <template>
-    <jet-authentication-card>
-        <template #logo>
-            <jet-authentication-card-logo />
-        </template>
-
-        <jet-validation-errors class="mb-4" />
-
-        <form @submit.prevent="submit">
-            <div>
-                <jet-label for="name" value="Name" />
-                <jet-input id="name" type="text" class="mt-1 block w-full" v-model="form.name" required autofocus autocomplete="name" />
-            </div>
-
-            <div class="mt-4">
-                <jet-label for="email" value="Email" />
-                <jet-input id="email" type="email" class="mt-1 block w-full" v-model="form.email" required />
-            </div>
-
-            <div class="mt-4">
-                <jet-label for="password" value="Password" />
-                <jet-input id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="new-password" />
-            </div>
-
-            <div class="mt-4">
-                <jet-label for="password_confirmation" value="Confirm Password" />
-                <jet-input id="password_confirmation" type="password" class="mt-1 block w-full" v-model="form.password_confirmation" required autocomplete="new-password" />
-            </div>
-
-            <div class="mt-4" v-if="$page.props.jetstream.hasTermsAndPrivacyPolicyFeature">
-                <jet-label for="terms">
-                    <div class="flex items-center">
-                        <jet-checkbox name="terms" id="terms" v-model:checked="form.terms" />
-
-                        <div class="ml-2">
-                            I agree to the <a target="_blank" :href="route('terms.show')" class="underline text-sm text-gray-600 hover:text-gray-900">Terms of Service</a> and <a target="_blank" :href="route('policy.show')" class="underline text-sm text-gray-600 hover:text-gray-900">Privacy Policy</a>
+    <div class="flex">
+        <!-- <div class="bg-black h-full w-2/3">
+            <div class="w-full h-full bg-gray-700"></div>
+        </div> -->
+        <jet-authentication-card class="bg-gradient-to-r from-red-600 to-yellow-300 h-full w-full">
+            <template #logo>
+                <p class="text-xl  text-white text-center font-extrabold m-auto my-12 border-white border-t border-b">
+                    CayraStyle.ID
+                </p>
+            </template>
+    
+            <jet-validation-errors class="mb-4" />
+    
+            <form @submit.prevent="submit" class="mx-6 py-4 text-sm">
+                <div>
+                    <jet-label for="name" value="Name" />
+                    <jet-input id="name" type="text" class="mt-1 block w-full" v-model="form.name" required autofocus autocomplete="name" />
+                </div>
+    
+                <div class="mt-4">
+                    <jet-label for="email" value="Email" />
+                    <jet-input id="email" type="email" class="mt-1 block w-full" v-model="form.email" required />
+                </div>
+    
+                <div class="mt-4">
+                    <jet-label for="password" value="Password" />
+                    <jet-input id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="new-password" />
+                </div>
+    
+                <div class="mt-4">
+                    <jet-label for="password_confirmation" value="Confirm Password" />
+                    <jet-input id="password_confirmation" type="password" class="mt-1 block w-full" v-model="form.password_confirmation" required autocomplete="new-password" />
+                </div>
+    
+                <div class="mt-4" v-if="$page.props.jetstream.hasTermsAndPrivacyPolicyFeature">
+                    <jet-label for="terms">
+                        <div class="flex items-center">
+                            <jet-checkbox name="terms" id="terms" v-model:checked="form.terms" />
+    
+                            <div class="ml-2">
+                                I agree to the <a target="_blank" :href="route('terms.show')" class="underline text-sm text-gray-600 hover:text-gray-900">Terms of Service</a> and <a target="_blank" :href="route('policy.show')" class="underline text-sm text-gray-600 hover:text-gray-900">Privacy Policy</a>
+                            </div>
                         </div>
-                    </div>
-                </jet-label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <inertia-link :href="route('login')" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    Already registered?
-                </inertia-link>
-
-                <jet-button class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Register
-                </jet-button>
-            </div>
-        </form>
-    </jet-authentication-card>
+                    </jet-label>
+                </div>
+    
+                <div class="flex items-center justify-end mt-4">
+                    <inertia-link :href="route('login')" class="underline text-sm text-gray-600 hover:text-gray-900">
+                        Already registered?
+                    </inertia-link>
+    
+                    <jet-button class="ml-4 bg-gradient-to-r from-red-600 to-yellow-300 border-none rounded-xl text-xs" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                        Register
+                    </jet-button>
+                </div>
+            </form>
+        </jet-authentication-card>
+    </div>
 </template>
 
 <script>
