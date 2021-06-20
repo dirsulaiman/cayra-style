8c0113cad39b67ed35b4a1cb9bbab094e626fef7
dirsulaiman <sulaiman.tsany@gmail.com>
2021-06-20 21:45:36 +0700

    logo

---
+++ resources/js/Layouts/BrandLogo.vue
@@ -0,0 +1,7 @@
+<template>
+    <inertia-link :href="'/'">
+        <p class="text-xl  text-white text-center font-extrabold m-auto my-12 border-white border-t border-b">
+            CayraStyle.ID
+        </p>
+    </inertia-link>
+</template>
