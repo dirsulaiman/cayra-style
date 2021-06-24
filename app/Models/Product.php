c35daa3bf6f87c174a92e456e0b73c4520926fca
dirsulaiman <sulaiman.tsany@gmail.com>
2021-06-24 21:59:47 +0700

    model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,20 +2,26 @@
 
 namespace App\Models;
 
+use App\Traits\Uuids;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     use HasFactory;
+    use Uuids;
 
-    public function details()
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    public function detail()
     {
-        return $this->hasOne(ProductDetail::class, 'detail_id');
+        return $this->hasOne(ProductDetail::class, 'product_id');
     }
 
     public function images()
     {
-        return $this->hasMany(ProductImage::class, 'image_id');
+        return $this->hasMany(ProductImage::class, 'product_id');
     }
 }
--- app/Models/ProductDetail.php
+++ app/Models/ProductDetail.php
@@ -2,15 +2,21 @@
 
 namespace App\Models;
 
+use App\Traits\Uuids;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ProductDetail extends Model
 {
     use HasFactory;
+    use Uuids;
+
+    public $incrementing = false;
+    protected $keyType = 'string';
 
     public function product()
     {
-        return $this->belongsTo(Product::class, 'product_id');
+        return $this->belongsTo(Product::class, 'detail_id');
     }
 }
