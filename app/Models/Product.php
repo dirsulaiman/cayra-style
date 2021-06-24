10354f85fa90ed9a77a163f9c7f6824b6ac1abb1
dirsulaiman <sulaiman.tsany@gmail.com>
2021-06-24 20:26:19 +0700

    model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    public function details()
+    {
+        return $this->hasOne(ProductDetail::class, 'detail_id');
+    }
+
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class, 'image_id');
+    }
 }
