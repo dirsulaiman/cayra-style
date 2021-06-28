f3564ada39cba0fd83ace8547f0c84eed544a45b
dirsulaiman <sulaiman.tsany@gmail.com>
2021-06-28 22:52:19 +0700

    product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,16 +8,13 @@ use Illuminate\Http\Request;
 use App\Models\Product as Model;
 use App\Models\ProductDetail;
 use App\Models\ProductImage;
+use App\Models\ProductLink;
 
 class ProductController extends Controller
 {
     public function index () {
         $model = Model::all();
         return Inertia::render('Products', ['products' => $model]);
-    }
-
-    public function detail (Request $request, $id) {
-        return;
     }
 
     public function dummy ($id, $image) {
@@ -38,10 +35,36 @@ class ProductController extends Controller
             'price' => $request->price,
             'description' => $request->description
         ]);
-        // check if image
+
         // check if detail
+        $detail = $request->detail;
+        if ($detail) {
+            ProductDetail::create([
+                'category' => $detail->category
+            ]);
+        }
+
+        // check if image
+        $images = $request->images;
+        foreach ($images as $image) {
+            ProductImage::create([
+                'product_id' => $model->id,
+                'filename' => $image->filename,
+                'dir' => $image->dir,
+                'is_thumbnail' => $image->is_thumbnail
+            ]);
+        }
+        
         // check if link
-        $model->save();
+        $links = $request->links;
+        foreach ($links as $link) {
+            ProductImage::create([
+                'product_id' => $model->id,
+                'name' => $link->name,
+                'link' => $link->link,
+                'link_alt' => $link->link_alt
+            ]);
+        }
 
     }
 
